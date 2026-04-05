c2df6e60161d9d9239a5eff6e236c4449970a446
sandem45 <116912414+SyntaxError0x2D@users.noreply.github.com>
2026-04-05 20:02:48 +0300

    Added bunch of parameters
    added bunch of parameters.
    N = polynomial degree
    acc = accuracy

--- half-derivative-xsquared.py
+++ half-derivative-xsquared.py
@@ -1,10 +1,17 @@
-h = 0.000001
-def f(x): return(x**2)
+from math import factorial
+from tqdm import tqdm
+h = 0.0000001
+N = 12
+def f(x): return(x**N)
 s = 0
 ak = 1
-for k in range(0, int(1/h)):
-    s += (-1)**k * ak * f(1-k*h)
+acc = 10**(-15)
+for k in tqdm(range(0, int(1/h))):
+    rs = (-1)**k * ak * f(1-k*h)
+    #print(rs)
+    if abs(rs) < acc: break
+    s += rs
     ak *= (1/2 - k)/(k+1)
 HD = s*h**(-1/2)
-pi = 64/9 * HD**(-2)
+pi = (4**N * factorial(N)**2 / (factorial(2*N) * HD))**2
 print(pi)
